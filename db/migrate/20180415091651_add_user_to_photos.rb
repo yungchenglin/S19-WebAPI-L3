e3929d81921a20c10866b9e9f80130ea7496ab67
yungcheng <yungcheng111023@gmail.com>
2018-04-15 17:18:06 +0800

    generate migration add_user_to_photos

---
+++ db/migrate/20180415091651_add_user_to_photos.rb
@@ -0,0 +1,4 @@
+class AddUserToPhotos < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415090813) do
+ActiveRecord::Schema.define(version: 20180415091651) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
