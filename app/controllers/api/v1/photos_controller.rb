085ed487b188ed78f9346482a89f4fa1be72951f
yungcheng <yungcheng111023@gmail.com>
2018-08-12 08:59:18 +0800

    format index and show output using jbuilder

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::PhotosController < ApiController
 
   def index
     @photos = Photo.all
-    render json: @photos
   end
 
   def show
@@ -13,11 +12,7 @@ class Api::V1::PhotosController < ApiController
         status: 400
       }
     else
-      render json: {
-        title: @photo.title,
-        date: @photo.date,
-        description: @photo.description
-      }
+      render "api/v1/photos/show"
     end
   end
 
